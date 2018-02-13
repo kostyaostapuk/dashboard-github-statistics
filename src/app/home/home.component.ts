4604ebbcbfc5a3c315885638b6bc33c1332420af
kostyaostapuk <ostapuk97@gmail.com>
2018-02-13 19:11:09 +0300

    fix

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,27 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-
+import { GithubService } from '../data/github.service';
+import { DataService } from '../data/data.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  githubCode: string;
-  clientID: string = 'f54825f8d3e236c4b3c2';
-  clientSecret: string ='756ff60fe66671b9adcc1b189055c84f478d3ae1';
-  callback: string='http://127.0.0.1:4200/#/auth';
+  constructor(private githubService: GithubService, private _data: DataService) { }
 
-  authLink: string=`https://github.com/login/oauth/authorize?client_id=${this.clientID}&client_secret=${this.clientSecret}&redirect_uri=${this.callback}`;
-
-  constructor(private route: ActivatedRoute) { }
+  auth(){ this.githubService.auth(); }
 
   ngOnInit() {
-    window.location.href=this.authLink;
-    this.route.params.subscribe(params=>{
-      this.githubCode=params['code'];
-      console.log(this.githubCode);
-    });
+    this.githubService.getToken();
   }
-
 }
